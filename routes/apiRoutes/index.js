fb4061355b9509aad51f9dd324cdaa8743832cfb
Sonarie <sonarie1980@gmail.com>
2022-03-13 10:03:34 -0500

    added info for api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,20 +1,38 @@
-//page that routes the user made notes to the notes page itself
 const router = require("express").Router();
-const path = require("path");
+const {
+  filterByQuery,
+  findById,
+  createNewNote,
+  validateNote,
+} = require("../../lib/notes.js");
+const { notes } = require("../../db/db.json");
 
-// router.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/public/notes.html"));
-// });
+router.get("/db", (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
 
-// router.post("/db", (req, res) => {
-//   req.body.id = notes.length.toString();
+router.get("/db/:id", (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
-//   if (!validateNote(req.body)) {
-//     res.status(400).send("The note is not properly formatted.");
-//   } else {
-//     const note = createNewNote(req.body, notes);
-//     res.json(note);
-//   }
-// });
+router.post("/db", (req, res) => {
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send("The note is not properly formatted.");
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
 
 module.exports = router;
